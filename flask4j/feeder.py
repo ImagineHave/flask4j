6da0049074a960ca155a683ec62700a16bd911ca
cwih <chris.williams@imagine-have.xyz>
2018-05-19 17:04:32 +0000

    upload and load

--- flask4j/feeder.py
+++ flask4j/feeder.py
@@ -1,8 +1,24 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify, request
 
 bp = Blueprint('feeder', __name__, url_prefix='/feeder')
 
-# a simple page that says hello
-@bp.route('/hello')
+# a simple page that says feeder
+@bp.route('/')
 def hello():
-    return 'Hello, World!'
+    return 'feeder'
+    
+# a simple page that says feeder
+@bp.route('/upload', methods=['POST'])
+def upload():
+    '''lets make a standard file to upload'''
+    
+    dataDict = request.get_json()
+    
+    print(dataDict)
+    return ('', 204)
+    
+# a simple page that says feeder
+@bp.route('/load', methods=['GET'])
+def load():
+    '''load the bible into the database'''
+    return 'load'
--- flask4j/reader.py
+++ flask4j/reader.py
@@ -3,6 +3,11 @@ from flask import Blueprint
 bp = Blueprint('reader', __name__, url_prefix='/reader')
 
 # a simple page that says hello
-@bp.route('/hello')
+@bp.route('/')
 def hello():
-    return 'Hello, World!'
+    return 'reader!'
+    
+# a simple page that says hello
+@bp.route('/quote')
+def quote():
+    return 'quote!'
